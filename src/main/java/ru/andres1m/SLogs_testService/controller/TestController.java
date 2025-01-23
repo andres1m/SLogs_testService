e196c04375cb065c058b1e804517b4c3c275f684
andres1m <andreusar11@gmail.com>
2025-01-23 14:10:09 +0500

    added Swagger

--- src/main/java/ru/andres1m/SLogs_testService/controller/TestController.java
+++ src/main/java/ru/andres1m/SLogs_testService/controller/TestController.java
@@ -1,5 +1,8 @@
 package ru.andres1m.SLogs_testService.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.andres1m.SLogs_testService.service.LogsMessageService;
@@ -9,11 +12,15 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/SLogs/test/")
+@Tag(name = "тестовый API", description = "API для тестирования SLogs")
 @AllArgsConstructor
 public class TestController {
     private final TestService testService;
     private final LogsMessageService logsMessageService;
 
+    @Operation(summary = "Имитировать запрос для получния данных", responses = {
+            @ApiResponse(responseCode = "200", description = "Получение данных")
+    })
     @GetMapping("/getSomeTestData")
     public String getSomeTestData(@RequestParam("dataId") String id) {
         try {
@@ -26,14 +33,17 @@ public class TestController {
         }
     }
 
+    @Operation(summary = "Имитировать запрос для отправки данных", responses = {
+            @ApiResponse(responseCode = "200", description = "Отправка данных")
+    })
     @PostMapping("/postSomeTestData")
     public String postSomeTestData(@RequestBody Map<String, String> jsonParams){
         try {
             testService.doTestGet();
-            logsMessageService.sendLogsMessage("Success to get data with id " + jsonParams.get("id"));
+            logsMessageService.sendLogsMessage("Success to post data with id " + jsonParams.get("id"));
             return "Success post: " + "id";
         } catch (Exception e) {
-            logsMessageService.sendLogsMessage("Fail to get data with id " + jsonParams.get("id"));
+            logsMessageService.sendLogsMessage("Fail to get post with id " + jsonParams.get("id"));
             return "Failed post";
         }
     }
